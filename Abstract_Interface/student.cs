665d39bf2924870f6b94678640aa7b9cb775ac1b
Jave <jinolupango11@gmail.com>
2021-09-30 21:23:08 +0800

    Add project files.

---
+++ Abstract_Interface/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Interface
+{
+    class student
+    {
+        public string fn { get; set; }
+        public string ln { get; set; }
+        public string mn { get; set; }
+    }
+}
